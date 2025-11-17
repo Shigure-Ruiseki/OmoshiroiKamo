e8e6892046cd84f83c63dd7bae4d6753c4ea822b
Ruisu <akitomoya2908@gmail.com>
2025-11-17 10:27:47 +0700

    Spotless

--- src/main/java/ruiseki/omoshiroikamo/api/energy/IEnergyTile.java
+++ src/main/java/ruiseki/omoshiroikamo/api/energy/IEnergyTile.java
@@ -1,7 +1,8 @@
 package ruiseki.omoshiroikamo.api.energy;
 
-import com.gtnewhorizon.gtnhlib.blockpos.BlockPos;
 import net.minecraftforge.common.util.ForgeDirection;
+
+import com.gtnewhorizon.gtnhlib.blockpos.BlockPos;
 
 public interface IEnergyTile {
 
