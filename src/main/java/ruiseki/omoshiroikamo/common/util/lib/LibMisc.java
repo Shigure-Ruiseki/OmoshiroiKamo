7c88744b40a1b90da1d1d22010e721f77cf0c25f
Ruisu <akitomoya2908@gmail.com>
2025-11-28 09:30:05 +0700

    Update dependencies

--- src/main/java/ruiseki/omoshiroikamo/common/util/lib/LibMisc.java
+++ src/main/java/ruiseki/omoshiroikamo/common/util/lib/LibMisc.java
@@ -13,8 +13,8 @@ public final class LibMisc {
     public static final String VERSION = Tags.VERSION;
     public static final String DEPENDENCIES = "required-after:gtnhlib@[0.8.15,);"
         + "required-after:structurelib@[1.4.23,);"
-        + "required-after:modularui2@[2.3.16-1.7.10,);"
-        + "after:Baubles|Expanded;"
+        + "required-after:modularui2@[2.3.18-1.7.10,);"
+        + "after:Baubles|Expanded@[2.2.2-1.7.10,);"
         + "after:Baubles;"
         + "after:NotEnoughItems;"
         + "after:Waila;"
