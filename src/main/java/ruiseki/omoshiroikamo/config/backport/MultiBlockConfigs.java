e807d2a4fe86f757f19bfa75a5a019fbea01e9a2
Chlorine0808 <to.denda@gmail.com>
2025-12-26 20:47:09 +0900

    Revive multiblock settings

--- src/main/java/ruiseki/omoshiroikamo/config/backport/MultiBlockConfigs.java
+++ src/main/java/ruiseki/omoshiroikamo/config/backport/MultiBlockConfigs.java
@@ -1,5 +1,6 @@
 package ruiseki.omoshiroikamo.config.backport;
 
+import com.gtnewhorizon.gtnhlib.config.Config;
 import com.gtnewhorizon.gtnhlib.config.ConfigException;
 import com.gtnewhorizon.gtnhlib.config.ConfigurationManager;
 
@@ -7,10 +8,12 @@ import ruiseki.omoshiroikamo.config.backport.multiblock.MultiblockWorldGenConfig
 import ruiseki.omoshiroikamo.config.backport.multiblock.QuantumBeaconConfig;
 import ruiseki.omoshiroikamo.config.backport.multiblock.QuantumExtractorConfig;
 import ruiseki.omoshiroikamo.config.backport.multiblock.SolarArrayConfig;
+import ruiseki.omoshiroikamo.core.lib.LibMisc;
+import ruiseki.omoshiroikamo.core.lib.LibResources;
 
-/**
- * Registration helper for multiblock config classes.
- */
+@Config.Comment("Main Multiblock Settings")
+@Config.LangKey(LibResources.CONFIG + "multiblockConfig")
+@Config(modid = LibMisc.MOD_ID, configSubDirectory = LibMisc.MOD_ID, category = "multiblock")
 public class MultiBlockConfigs {
 
     public static void registerConfig() throws ConfigException {
