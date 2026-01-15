303fc5830259c54b92d6a9f1d0a3dac0decdee38
Ruisu <akitomoya2908@gmail.com>
2026-01-15 21:21:29 +0700

    fix bug

--- src/main/java/ruiseki/omoshiroikamo/module/backpack/client/gui/container/BackPackContainer.java
+++ src/main/java/ruiseki/omoshiroikamo/module/backpack/client/gui/container/BackPackContainer.java
@@ -565,6 +565,7 @@ public class BackPackContainer extends ModularContainer {
             this.resetDrag();
         }
 
+        detectAndSendChanges();
         return Platform.EMPTY_STACK;
     }
 
