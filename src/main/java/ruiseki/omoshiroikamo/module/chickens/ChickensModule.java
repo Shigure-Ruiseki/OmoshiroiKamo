f386861f3874f502ffff9ca8a2604b641bf763e8
Chlorine0808 <to.denda@gmail.com>
2026-02-26 21:36:06 +0900

    fix?

--- src/main/java/ruiseki/omoshiroikamo/module/chickens/ChickensModule.java
+++ src/main/java/ruiseki/omoshiroikamo/module/chickens/ChickensModule.java
@@ -1,15 +1,9 @@
 package ruiseki.omoshiroikamo.module.chickens;
 
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.fluids.FluidContainerRegistry;
-import net.minecraftforge.fluids.FluidStack;
-
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import ruiseki.omoshiroikamo.OmoshiroiKamo;
-import ruiseki.omoshiroikamo.api.entity.chicken.LiquidEggRegistry;
-import ruiseki.omoshiroikamo.api.entity.chicken.LiquidEggRegistryItem;
 import ruiseki.omoshiroikamo.config.backport.BackportConfigs;
 import ruiseki.omoshiroikamo.core.helper.MinecraftHelpers;
 import ruiseki.omoshiroikamo.core.init.ModModuleBase;
@@ -18,7 +12,6 @@ import ruiseki.omoshiroikamo.module.chickens.client.util.TextureGenerator;
 import ruiseki.omoshiroikamo.module.chickens.common.init.ChickensBlocks;
 import ruiseki.omoshiroikamo.module.chickens.common.init.ChickensItems;
 import ruiseki.omoshiroikamo.module.chickens.common.init.ChickensRecipes;
-import ruiseki.omoshiroikamo.module.chickens.common.item.ItemLiquidEgg;
 import ruiseki.omoshiroikamo.module.chickens.registries.ModChickens;
 
 public class ChickensModule extends ModModuleBase {
@@ -55,21 +48,15 @@ public class ChickensModule extends ModModuleBase {
 
     @Override
     public void init(FMLInitializationEvent event) {
-        ItemStack emptyEgg = ChickensItems.EMPTY_EGG.newItemStack();
-
-        for (LiquidEggRegistryItem egg : LiquidEggRegistry.getAll()) {
-            ItemStack filled = ChickensItems.LIQUID_EGG.newItemStack(1, egg.getId());
-            ItemLiquidEgg.setFluid(filled, new FluidStack(egg.getFluid(), 1000));
-            FluidContainerRegistry.registerFluidContainer(new FluidStack(egg.getFluid(), 1000), filled, emptyEgg);
-        }
-
         ModChickens.init();
-        ChickensRecipes.init();
     }
 
     @Override
     public void postInit(FMLPostInitializationEvent event) {
         ModChickens.postInit();
-        TextureGenerator.generateCustomChickenTextures();
+        ChickensRecipes.init();
+        if (MinecraftHelpers.isClientSide()) {
+            TextureGenerator.generateCustomChickenTextures();
+        }
     }
 }
