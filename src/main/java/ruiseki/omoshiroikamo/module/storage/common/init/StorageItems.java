4dd6ab0027578902a7cdfce5539ce5cec3a5af78
Ruisu <akitomoya2908@gmail.com>
2026-03-27 23:00:21 +0700

    add texture

--- src/main/java/ruiseki/omoshiroikamo/module/storage/common/init/StorageItems.java
+++ src/main/java/ruiseki/omoshiroikamo/module/storage/common/init/StorageItems.java
@@ -6,19 +6,37 @@ import net.minecraft.item.ItemStack;
 import ruiseki.omoshiroikamo.core.common.util.Logger;
 import ruiseki.omoshiroikamo.core.item.IItem;
 import ruiseki.omoshiroikamo.core.item.ItemOK;
+import ruiseki.omoshiroikamo.module.storage.common.item.ItemAdvancedFeedingUpgrade;
+import ruiseki.omoshiroikamo.module.storage.common.item.ItemAdvancedFilterUpgrade;
+import ruiseki.omoshiroikamo.module.storage.common.item.ItemAdvancedMagnetUpgrade;
+import ruiseki.omoshiroikamo.module.storage.common.item.ItemAdvancedPickupUpgrade;
+import ruiseki.omoshiroikamo.module.storage.common.item.ItemAdvancedVoidUpgrade;
 import ruiseki.omoshiroikamo.module.storage.common.item.ItemCraftingUpgrade;
 import ruiseki.omoshiroikamo.module.storage.common.item.ItemFeedingUpgrade;
 import ruiseki.omoshiroikamo.module.storage.common.item.ItemFilterUpgrade;
+import ruiseki.omoshiroikamo.module.storage.common.item.ItemMagnetUpgrade;
+import ruiseki.omoshiroikamo.module.storage.common.item.ItemPickupUpgrade;
 import ruiseki.omoshiroikamo.module.storage.common.item.ItemStackUpgrade;
+import ruiseki.omoshiroikamo.module.storage.common.item.ItemUpgrade;
+import ruiseki.omoshiroikamo.module.storage.common.item.ItemVoidUpgrade;
 
 public enum StorageItems {
 
     // spotless: off
 
+    BASE_UPGRADE(new ItemUpgrade<>()),
     STACK_UPGRADE(new ItemStackUpgrade()),
     CRAFTING_UPGRADE(new ItemCraftingUpgrade()),
+    MAGNET_UPGRADE(new ItemMagnetUpgrade()),
+    ADVANCED_MAGNET_UPGRADE(new ItemAdvancedMagnetUpgrade()),
+    FEEDING_UPGRADE(new ItemFeedingUpgrade()),
+    ADVANCED_FEEDING_UPGRADE(new ItemAdvancedFeedingUpgrade()),
+    PICKUP_UPGRADE(new ItemPickupUpgrade()),
+    ADVANCED_PICKUP_UPGRADE(new ItemAdvancedPickupUpgrade()),
+    VOID_UPGRADE(new ItemVoidUpgrade()),
+    ADVANCED_VOID_UPGRADE(new ItemAdvancedVoidUpgrade()),
     FILTER_UPGRADE(new ItemFilterUpgrade()),
-    FEEDING_UPGRADE(new ItemFeedingUpgrade())
+    ADVANCED_FILTER_UPGRADE(new ItemAdvancedFilterUpgrade()),
 
     ;
     // spotless: on
